6ce9e3b339c13326d4c416e2bba514e10dbbb66f
Terry Fleury <tfleury@illinois.edu>
2010-03-03 22:35:32 +0000

    Finished with the first functional version of the "requestidp" script. Everything is working now, but I need to change the email address for whom is going to receive the request for a new IdP.

--- requestidp.php
+++ requestidp.php
@@ -6,7 +6,6 @@ require_once('include/shib.php');
 require_once('include/util.php');
 
 $submit = getPostVar('submit');
-$csrf = new csrf();
 $validator = new EmailAddressValidator();
 define('DEFAULT_OPTION_TEXT','-- Choose one -or- Type one in below --');
 
@@ -20,7 +19,7 @@ if (strlen($submit) > 0) {
     }
 }
 
-/* If the CSRF cookie was good and the user clickecd the "Submit" *
+/* If the CSRF cookie was good and the user clicked the "Submit" *
  * button then read in the various form elements and verify that  *
  * the form values are non-empty (or at least make sense).        */
 if ($submit == 'Submit') {
@@ -37,21 +36,23 @@ if ($submit == 'Submit') {
             (($selectIdP != DEFAULT_OPTION_TEXT) ||
              (strlen($otherIdP) > 2))) {
         /* Everything is okay! Send email request and print "Thank you!" */
-        printFormSubmitted($yourName,$emailAddr,$selectIdP,$otherIdP,$comments);
+        printRequestSubmitted($yourName,$emailAddr,
+                              $selectIdP,$otherIdP,$comments);
     } else {
         /* User entered some blank (or invalid) fields.  Show the *
          * Main Form page again, with their inputs, PLUS the      *
          * places that there are errors.                          */
-        printMainForm(true,$yourName,$emailAddr,$selectIdP,$otherIdP,$comments);
+        printRequestForm(true,$yourName,$emailAddr,
+                         $selectIdP,$otherIdP,$comments);
     }
 
 } else {   
     /* Default condition, print the Main form */
-    printMainForm();
+    printRequestForm();
 }
 
 /************************************************************************
- * Function   : printMainForm                                           *
+ * Function   : printRequestForm                                        *
  * Parameters : (1) Boolean for if we should try to verify that there   *
  *                  are valid entries in the various fields.            *
  *              (2) The input Name of the user.                         *
@@ -68,54 +69,54 @@ if ($submit == 'Submit') {
  * error icon is displayed next to that field, and the user is asked    *
  * to fix the errors before submitting again.                           *
  ************************************************************************/
-function printMainForm($verify=false,$yourName='',$emailAddr='',
-                       $selectIdP='',$otherIdP='',$comments='') 
+function printRequestForm($verify=false,$yourName='',$emailAddr='',
+                          $selectIdP='',$otherIdP='',$comments='') 
 {
     global $validator;
-    global $csrf;
+
+    $csrf = new csrf();
+    $csrf->setTheCookie();
 
     $goterror = false;  /* Did we find any errors in the form? */
 
     $idps = getInCommonIdPs();
 
-    $csrf->setTheCookie();
-
     printHeader('Request Identity Provider');
 
     echo '
-      <div id="pageHeader">
-        <h1><span>Request Identity Provider</span></h1>
-        <h2><span>Help Us Add Your Organization To Our Service</span></h2>
-      </div>
+    <div id="pageHeader">
+      <h1><span>Request Identity Provider</span></h1>
+      <h2><span>Help Us Add Your Organization To Our Service</span></h2>
+    </div>
 
-      <div id="summary">
-        <p class="p1"><span>Thank you for your interest in the CILogon Service.
-          The CILogon Service Provider (<acronym 
-          title="Service Provider">SP</acronym>) is a member of the <a
-          target="_blank" href="http://www.incommonfederation.org/">InCommon
-          Federation</a>, which brings together over 200 educational
-          institutions and professional organizations.  However, before you can
-          utilize the CILogon Service, your organization\'s Identity Provider
-          (<acronym title="Identity Provider">IdP</acronym>) must release
-          specific user attributes to our site.</span></p>
-        <p class="p2"><span>You can help us by letting us know that you are
-          interested in using our service.  Please enter your contact
-          information and select your organization below.  We will contact
-          the appropriate administrators in an effort to have your
-          organizations\'s <acronym title="Identity Provider">IdP</acronym>
-          operate with our <acronym 
-          title="Service Provider">SP</acronym>.</span></p>
-      </div>
+    <div id="summaryDiv">
+      <p class="p1"><span>Thank you for your interest in the CILogon Service.
+        The CILogon Service Provider (<acronym 
+        title="Service Provider">SP</acronym>) is a member of the <a
+        target="_blank" href="http://www.incommonfederation.org/">InCommon
+        Federation</a>, which brings together over 200 educational
+        institutions and professional organizations.  However, before you can
+        utilize the CILogon Service, your organization\'s Identity Provider
+        (<acronym title="Identity Provider">IdP</acronym>) must release
+        specific user attributes to our site.</span></p>
+      <p class="p2"><span>You can help us by letting us know that you are
+        interested in using our service.  Please enter your contact
+        information and select your organization below.  We will contact
+        the appropriate administrators in an effort to have your
+        organizations\'s <acronym title="Identity Provider">IdP</acronym>
+        operate with our <acronym 
+        title="Service Provider">SP</acronym>.</span></p>
+    </div>
 
-      <div id="contactForm">
-        <form action="requestidp.php" method="post" class="requestForm">
-        <fieldset>
-        <legend><span>Contact Information</span></legend>
-        <ol>
-        <li>
-        <label for="yourName">Your Name</label>
-        <input id="yourName" name="yourName" class="text" type="text" 
-         size="50" maxlength="80" value="' . $yourName . '" />';
+    <div id="contactForm">
+      <form action="requestidp.php" method="post" class="requestForm">
+      <fieldset>
+      <legend><span>Contact Information</span></legend>
+      <ol>
+      <li>
+      <label for="yourName">Your Name</label>
+      <input id="yourName" name="yourName" class="text" type="text" 
+       size="50" maxlength="80" value="' . $yourName . '" />';
 
     if (($verify) && (strlen($yourName) <= 2)) {
         printErrorIcon();
@@ -123,11 +124,11 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
     }
 
     echo '
-        </li>
-        <li>
-        <label for="emailAddr">Email Address</label>
-        <input id="emailAddr" name="emailAddr" class="text" type="text" 
-         size="50" maxlength="80" value="' . $emailAddr . '" />';
+      </li>
+      <li>
+      <label for="emailAddr">Email Address</label>
+      <input id="emailAddr" name="emailAddr" class="text" type="text" 
+       size="50" maxlength="80" value="' . $emailAddr . '" />';
 
     if (($verify) && (!$validator->check_email_address($emailAddr))) {
         printErrorIcon();
@@ -135,20 +136,20 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
     }
 
     echo '
-        </li>
-        </ol>
-        </fieldset>
-        <fieldset>
-        <legend><span>Identity Provider</span></legend>
-        <ol>
-        <li>
-        <label for="selectIdP">Select an IdP</label>
-        <select name="selectIdP" id="selectIdP" class="select">
-        <option value="' . DEFAULT_OPTION_TEXT . '"';
+      </li>
+      </ol>
+      </fieldset>
+      <fieldset>
+      <legend><span>Identity Provider</span></legend>
+      <ol>
+      <li>
+      <label for="selectIdP">Select an IdP</label>
+      <select name="selectIdP" id="selectIdP" class="select">
+      <option value="' . DEFAULT_OPTION_TEXT . '"';
 
     if (($selectIdP == DEFAULT_OPTION_TEXT) ||
         (strlen($selectIdP) == 0)) {
-        echo ' selected';
+        echo ' selected="selected"';
     }
         
     echo '>' . DEFAULT_OPTION_TEXT . '</option>
@@ -158,13 +159,13 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
         echo '
         <option value="'.$value.'"';
         if ($value == $selectIdP) {
-            echo ' selected';
+            echo ' selected="selected"';
         }
         echo '>'.$value.'</option>';
     }
 
     echo '
-        </select>';
+      </select>';
 
     if (($verify) && ($selectIdP == DEFAULT_OPTION_TEXT) &&
         (strlen($otherIdP) <= 2)) {
@@ -173,22 +174,22 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
     }
 
     echo '
-        </li>
-        <li>
-        <label for="otherIdP">Other IdP</label>
-        <input id="otherIdP" name="otherIdP" class="text" type="text" 
-         size="50" maxlength="80" value="' . $otherIdP . '" />
-        </li>
-        <li>
-        <label for="comments">(Optional) Comments</label>
-        <textarea id="comments" name="comments" class="textarea" 
-        cols="50" rows="5">' .  $comments . '</textarea>
-        </li>
-        </ol>
-        </fieldset>
-        <fieldset class="submit">
-        <input class="submit" type="submit" name="submit" value="Submit"
-        />';
+      </li>
+      <li>
+      <label for="otherIdP">Other IdP</label>
+      <input id="otherIdP" name="otherIdP" class="text" type="text" 
+       size="50" maxlength="80" value="' . $otherIdP . '" />
+      </li>
+      <li>
+      <label for="comments">(Optional) Comments</label>
+      <textarea id="comments" name="comments" class="textarea" 
+      cols="50" rows="5">' .  $comments . '</textarea>
+      </li>
+      </ol>
+      </fieldset>
+      <fieldset class="submit">
+      <input class="submit" type="submit" name="submit" value="Submit"
+      />';
 
     if ($goterror) {
         echo '
@@ -197,21 +198,17 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
     }
 
     echo '
-        </fieldset>
-    ';
-
-    echo $csrf->getHiddenFormElement();
-
-    echo '
-        </form>
-      </div>
+      </fieldset>
+    ' .  $csrf->getHiddenFormElement() . '
+      </form>
+    </div>
     ';
 
     printFooter();
 }
 
 /************************************************************************
- * Function   : printFormSubmitted                                      *
+ * Function   : printRequestSubmitted                                   *
  * Parameters : (1) The input Name of the user.                         *
  *              (2) The input email address of the user.                *
  *              (3) The selected InCommon IdP.                          *
@@ -222,7 +219,8 @@ function printMainForm($verify=false,$yourName='',$emailAddr='',
  * an email to "info@cilogon.org" and then prints out a "Thank You"     *
  * page to the user.                                                    *
  ************************************************************************/
-function printFormSubmitted($yourName,$emailAddr,$selectIdP,$otherIdP,$comments)
+function printRequestSubmitted($yourName,$emailAddr,
+                               $selectIdP,$otherIdP,$comments)
 {
     $loggit = new loggit('mail','tfleury@illinois.edu');
     $loggit->info("
@@ -238,27 +236,27 @@ Comments      = $comments
     printHeader('Identity Provider Requested');
 
     echo '
-      <div id="pageHeader">
-        <h1><span>Request Received</span></h1>
-        <h2><span>Thank You For Your Interest In the CILogon Service</span></h2>
-      </div>
+    <div id="pageHeader">
+      <h1><span>Request Received</span></h1>
+      <h2><span>Thank You For Your Interest In the CILogon Service</span></h2>
+    </div>
 
-      <div id="summary">
-        <p class="p1"><span>Thank you!  Your request for a new Identity
-        Provider has been sent to the CILogon Service development team.
-        We will contact the appropriate administrators at your organization
-        and ask that they release the necessary attributes to our Service
-        Provider (<acronym title="Service Provider">SP</acronym>).</span></p>
-        <p class="p2"><span>Please allow several business days for your
-        organization\'s administrators to reply to your request.  We will
-        contact you as soon as we have any information.  Thank you again for
-        your request.</span></p>
-        <p class="p3"><span>If you have any further questions, please
-        contact us at <a
-        href="mailto:info@cilogon.org">info@cilogon.org</a>.  You can now <a
-        href="https://cilogon.org/">return to the main CILogon Service
-        page</a>.</span></p>
-      </div>
+    <div id="summaryDiv">
+      <p class="p1"><span>Thank you!  Your request for a new Identity
+      Provider has been sent to the CILogon Service development team.
+      We will contact the appropriate administrators at your organization
+      and ask that they release the necessary attributes to our Service
+      Provider (<acronym title="Service Provider">SP</acronym>).</span></p>
+      <p class="p2"><span>Please allow several business days for your
+      organization\'s administrators to reply to your request.  We will
+      contact you as soon as we have any information.  Thank you again for
+      your request.</span></p>
+      <p class="p3"><span>If you have any further questions, please
+      contact us at <a
+      href="mailto:info@cilogon.org">info@cilogon.org</a>.  You can now <a
+      href="https://cilogon.org/">return to the main CILogon Service
+      page</a>.</span></p>
+    </div>
     ';
 
     printFooter();
