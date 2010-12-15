25ef44dbb168def92f9f7218f4083a7da1dd1cd9
Terry Fleury <tfleury@illinois.edu>
2010-12-15 23:41:26 +0000

    Added the new Activation Code box for use with third-party clients, like the gridshibca-client.py python program.

--- index-site.php
+++ index-site.php
@@ -66,6 +66,17 @@ switch ($submit) {
         }
     break; // End case 'Go Back' / 'Proceed'
 
+    case 'Get New Activation Code':
+        // Verify the PHP session contains valid info
+        if (verifyCurrentSession()) {
+            generateToken();
+            printMainPage();
+        } else { // Otherwise, redirect to the 'Welcome' page
+            unsetGetUserSessionVars();
+            printLogonPage();
+        }
+    break; // End case 'Get New Activation Code'
+
     default: // No submit button clicked nor PHP session submit variable set
         /* If both the "keepidp" and the "providerId" cookies were set *
          * (and the providerId is a whitelisted IdP or valid OpenID    *
@@ -128,6 +139,7 @@ function printMainPage()
 
     $downloadcerttext = "Download a certificate to your local computer. Clicking this button should launch a Java Web Start (JWS) application, which requires Java to be installed on your computer and enabled in your web browser.";
     $logofftext = "End your CILogon session and return to the welcome page. Note that this will not log you out at your identity provider.";
+    $generatetokentext = "Get a new one-time-use activation code for CILogon-enabled applications.";
 
     $log->info('Get And Use Certificate page hit.');
 
@@ -208,10 +220,10 @@ function printMainPage()
       <input type="text" name="certlifetime" id="certlifetime" 
       title="', $lifetimetext , 
       '" class="helpcursor" value="' , $certlifetime ,
-      '" size="8" maxlength="8" disabled="true"/> 
+      '" size="8" maxlength="8" disabled="disabled"/> 
       <select title="' , $lifetimetext , 
       '" class="helpcursor" id="certmultiplier" name="certmultiplier"
-      disabled="true">
+      disabled="disabled">
       <option value="3600"' , 
           (($certmultiplier==3600) ? ' selected="selected"' : '') , 
           '>hours</option>
@@ -241,6 +253,7 @@ function printMainPage()
 
       echo '
       <script type="text/javascript">
+//<![CDATA[
       function init() {
         var certlifetimeinput = document.getElementById("certlifetime");
         if (certlifetimeinput !== null) {
@@ -253,8 +266,8 @@ function printMainPage()
         var mayneedjavapara = document.getElementById("mayneedjava");
         if (mayneedjavapara !== null) {
           if (!deployJava.isWebStartInstalled("1.6.0")) {
-            mayneedjavapara.style.display = "inline";
-            mayneedjavapara.style.height = "auto";
+            mayneedjavapara.style.display = "block";
+            mayneedjavapara.style.height = "1.5em";
             mayneedjavapara.style.width = "auto";
             mayneedjavapara.style.lineHeight = "auto";
             mayneedjavapara.style.overflow = "visible";
@@ -262,6 +275,7 @@ function printMainPage()
         }
         return true;
       }
+//]]>
       </script>
       <noscript>
       <div class="nojs smaller">
@@ -269,7 +283,91 @@ function printMainPage()
       </div>
       </noscript>
       </form>
-    </div>
+    </div> <!-- certactionbox -->
+
+    <div class="tokenactionbox">
+    ';
+    
+    /* Print out the box for the Certificate Token, which can be used by *
+     * the GridShib-CA python client to download a certificate.  The     *
+     * JavaScript in the following section is a countdown timer showing  *
+     * the remaining validity time for the given token.                  */
+
+    printFormHead($scriptdir);
+
+    validateToken();
+    $tokenvalue = getSessionVar('tokenvalue');
+    $tokenexpire = getSessionVar('tokenexpire');
+    $tokenvaluetext = '';
+    if ((strlen($tokenvalue) > 0) && (strlen($tokenexpire) > 0)) {
+        $tokenvalue = 'Activation&nbsp;Code: ' . $tokenvalue;
+        $tokenvaluetext = 'Copy and paste the one-time-use activation code into your CILogon-enabled application to download a certificate.';
+    } else {
+        $tokenvalue = 'For CILogon-enabled Applications:';
+        $tokenvaluetext = 'Click the button below to display a one-time-use activation code for CILogon-enabled applications. You can copy and paste this code into the application to download a certificate.';
+    }
+    if ((strlen($tokenexpire) > 0) && ($tokenexpire > 0)) {
+        $expire = $tokenexpire - time();
+        $minutes = floor($expire % 3600 / 60);
+        $seconds = $expire % 60;
+        $tokenexpire = 'Expires: ' . 
+            sprintf("%02dm:%02ds",$minutes,$seconds);
+    } else {
+        $tokenexpire = '';
+    }
+
+    echo '
+      <p id="tokenvalue" class="helpcursor" title="' , 
+          $tokenvaluetext , '">' , $tokenvalue , '</p>
+      <p id="tokenexpire">' , $tokenexpire , '</p>
+      <script type="text/javascript">
+//<![CDATA[
+        function countdown() {
+          var tokenexpire = document.getElementById(\'tokenexpire\');
+          if (tokenexpire !== null) {
+            var expiretext = tokenexpire.innerHTML;
+            if ((expiretext !== null) && (expiretext.length > 0)) {
+              var matches = expiretext.match(/\d+/g);
+              if (matches.length == 2) {
+                var minutes = parseInt(matches[0],10);
+                var seconds = parseInt(matches[1],10);
+                if ((minutes > 0) || (seconds > 0)) {
+                  seconds -= 1;
+                  if (seconds < 0) {
+                    minutes -= 1;
+                    if (minutes >= 0) {
+                      seconds = 59;
+                    }
+                  }
+                  if ((seconds > 0) || (minutes > 0)) {
+                    tokenexpire.innerHTML = "Expires: " + 
+                      ((minutes < 10) ? "0" : "") + minutes + "m:" +
+                      ((seconds < 10) ? "0" : "") + seconds + "s";
+                      setTimeout(countdown,1000);
+                  } else {
+                    tokenexpire.innerHTML = "";
+                    var tokenvalue = document.getElementById(\'tokenvalue\');
+                    if (tokenvalue !== null) {
+                      tokenvalue.innerHTML = 
+                        "For CILogon-enabled Applications:";
+                      tokenvalue.title = "Click the button below to display a one-time-use activation code for CILogon-enabled applications. You can copy and paste this code into the application to download a certificate.";
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        countdown();
+//]]>
+      </script>
+
+      <p>
+      <input type="submit" name="submit" class="submit helpcursor" 
+      title="' , $generatetokentext , '" value="Get New Activation Code" />
+      </p>
+      </form>
+    </div> <!-- tokenactionbox -->
 
     <div class="actionbox">
     ';
@@ -277,13 +375,99 @@ function printMainPage()
     printFormHead($scriptdir);
 
     echo '
+      <p>
       <input type="submit" name="submit" class="submit helpcursor" 
       title="' , $logofftext , '" value="Log Off" />
+      </p>
       </form>
     </div>
     </div>
     ';
     printFooter();
+}
+
+/************************************************************************
+ * Function   : generateToken                                           *
+ * This function is called when the user clicks the "Generate New       *
+ * Token" button.  It basically does the same thing as when the user    *
+ * clicks the "Download Certificate", i.e. it calls the function to     *
+ * create a .jnlp file.  However, this method uses 'curl' to slurp in   *
+ * the resulting .jnlp file and scans for the AuthenticationToken       *
+ * in the file.  This is printed out to the user.  The token can be     *
+ * used by the GridShib-CA python client to fetch a certificate.        *
+ ************************************************************************/
+function generateToken() {
+    global $csrf;
+
+    $tokenvalue = '';
+    $gridshibconf = parseGridShibConf();
+
+    $ch = curl_init();
+    if ($ch != false) {
+        $url = 'https://' . HOSTNAME . preg_replace('/^\s*=\s*/','',
+            $gridshibconf['root']['GridShibCAURL']) . 'shibCILaunchGSCA.jnlp';
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_TIMEOUT,30);
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch,CURLOPT_HEADER,false);
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,1);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,"CSRFProtection=" .
+            $csrf->getTokenValue());
+        curl_setopt($ch,CURLOPT_COOKIE,'PHPSESSID=' .
+            getCookieVar('PHPSESSID') . '; CSRFProtection=' .
+            $csrf->getTokenValue() . ';');
+
+        // Must close PHP session file so GridShib-CA can read it.
+        session_write_close();
+        $output = curl_exec($ch);
+        if (!empty($output)) {
+            $httpcode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+            if ($httpcode == 200) {
+                if (preg_match('/AuthenticationToken = ([^<]+)/',
+                    $output,$match)) {
+                    $tokenvalue = $match[1];
+                }
+            }
+        }
+        curl_close($ch);
+
+        session_start();
+        if (strlen($tokenvalue) > 0) {
+            setOrUnsetSessionVar('tokenvalue',$tokenvalue);
+            $tokenlifetime = preg_replace('/^\s*=\s*/','',
+                $gridshibconf['root']['Session']['CredentialRetrieverClientLifetime']);
+            if ((strlen($tokenlifetime) <= 0) || ($tokenlifetime == 0)) {
+                $tokenlifetime = 300;
+            }
+            setOrUnsetSessionVar('tokenexpire',time()+$tokenlifetime);
+        }
+    }
+}
+
+/************************************************************************
+ * Function   : validateToken                                           *
+ * This function is called just before the certificate token is printed *
+ * out to HTML.  It checks to see if the PHP session value              *
+ * 'tokenexpire' (which is the expiration time in Unix seconds) is      *
+ * greater than the current time().  If not, then it unsets the PHP     *
+ * session variables 'tokenexpire' and 'tokenvalue'.                    *
+ ************************************************************************/
+function validateToken() {
+    $tokenvalue = getSessionVar('tokenvalue');
+    $tokenexpire = getSessionVar('tokenexpire');
+
+    /* If there is a tokenexpire value, check against current time */
+    if ((strlen($tokenexpire) > 0) && ($tokenexpire > 0)) {
+        if (time() > $tokenexpire) {
+            unsetSessionVar('tokenvalue');
+            unsetSessionVar('tokenexpire');
+        }
+    } else {
+        unsetSessionVar('tokenvalue');
+    }
 }
 
 ?>
