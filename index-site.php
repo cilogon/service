1792213be3d8ccfdeec892c210a645b085a608e9
Terry Fleury <tfleury@illinois.edu>
2011-05-19 19:59:06 +0000

    Move generateP12 from main index-site.php to content.php, so it can be called by the secure/getuser/ script.

--- index-site.php
+++ index-site.php
@@ -4,7 +4,6 @@ require_once('include/util.php');
 require_once('include/autoloader.php');
 require_once('include/content.php');
 require_once('include/shib.php');
-require_once('include/myproxy.php');
 
 /* Read in the whitelist of currently available IdPs. */
 $white = new whitelist();
@@ -135,8 +134,7 @@ switch ($submit) {
  * and get rerouted to the Shibboleth protected service script, or the  *
  * OpenID script.                                                       *
  ************************************************************************/
-function printLogonPage()
-{
+function printLogonPage() {
     global $log;
 
     $log->info('Welcome page hit.');
@@ -163,8 +161,7 @@ function printLogonPage()
  * This function prints out the HTML for the main page where the user   *
  * can download a certificate or generate an Activation Code.           *
  ************************************************************************/
-function printMainPage()
-{
+function printMainPage() {
     global $log;
 
     $log->info('Get And Use Certificate page hit.');
@@ -741,135 +738,6 @@ function printLogOff() {
 }
 
 /************************************************************************
- * Function   : generateP12                                             *
- * This function is called when the user clicks the "Get New            *
- * Certificate" button. It first reads in the password fields and       *
- * verifies that they are valid (i.e. they are long enough and match).  *
- * Then it gets a credential from the MyProxy server and converts that  *
- * certificate into a PKCS12 which is written to disk.  If everything   *
- * succeeds, the temporary pkcs12 directory and lifetime is saved to    *
- * the 'p12' PHP session variable, which is read later when the Main    *
- * Page HTML is shown.                                                  *
- ************************************************************************/
-function generateP12() {
-    global $log;
-
-    /* Get the entered p12lifetime and p12multiplier and set the cookies. */
-    $maxlifetime = 9516; // In hours = 13 months
-    $p12lifetime   = getPostVar('p12lifetime');
-    $p12multiplier = getPostVar('p12multiplier');
-    $lifetime = $p12lifetime * $p12multiplier;
-    if ($lifetime <= 0) { // In case user entered negative number
-        $lifetime = $maxlifetime;
-        $p12lifetime = 13;
-        $p12multiplier = 732;
-    } elseif ($lifetime > $maxlifetime) { // Set max value based on multiplier
-        $lifetime = $maxlifetime;
-        if ($p12multiplier == 1) {
-            $p12lifetime = 9516;  // 13 months in hours
-        } elseif ($p12multiplier == 24) {
-            $p12lifetime = 396.5; // 13 months in days
-        } else { 
-            $p12lifetime = 13;    // 13 months (in months)
-            $p12multiplier = 732;
-        }
-    }
-    setcookie('p12lifetime',$p12lifetime,time()+60*60*24*365,'/','',true);
-    setcookie('p12multiplier',$p12multiplier,time()+60*60*24*365,'/','',true);
-    setSessionVar('p12lifetime',$p12lifetime);
-    setSessionVar('p12multiplier',$p12multiplier);
-
-    /* Verify that the password is at least 12 characters long. */
-    $password1 = getPostVar('password1');
-    $password2 = getPostVar('password2');
-    if (strlen($password1) < 12) {   
-        setSessionVar('p12error',
-            'Password must have at least 12 characters.');
-        return; // SHORT PASSWORD - NO FURTHER PROCESSING NEEDED!
-    }
-
-    /* Verify that the two password entry fields matched. */
-    if ($password1 != $password2) {
-        setSessionVar('p12error','Passwords did not match.');
-        return; // MISMATCHED PASSWORDS - NO FURTHER PROCESSING NEEDED!
-    }
-
-    /* Set the port based on the Level of Assurance */
-    $port = 7512;
-    $loa = getSessionVar('loa');
-    if ($loa == 'http://incommonfederation.org/assurance/silver') {
-        $port = 7514;
-    } elseif ($loa == 'openid') {
-        $port = 7516;
-    }
-
-    $dn = getSessionVar('dn');
-    if (strlen($dn) > 0) {
-        /* Attempt to fetch a credential from the MyProxy server */
-        $cert = getMyProxyCredential($dn,'','myproxy.cilogon.org',
-            $port,$lifetime,'/var/www/config/hostcred.pem','');
-
-        /* The 'openssl pkcs12' command is picky in that the private  *
-         * key must appear BEFORE the public certificate. But MyProxy *
-         * returns the private key AFTER. So swap them around.        */
-        $cert2 = '';
-        if (preg_match('/-----BEGIN CERTIFICATE-----([^-]+)' . 
-                       '-----END CERTIFICATE-----[^-]*' . 
-                       '-----BEGIN RSA PRIVATE KEY-----([^-]+)' .
-                       '-----END RSA PRIVATE KEY-----/',$cert,$match)) {
-            $cert2 = "-----BEGIN RSA PRIVATE KEY-----" .
-                     $match[2] . "-----END RSA PRIVATE KEY-----\n".
-                     "-----BEGIN CERTIFICATE-----" .
-                     $match[1] . "-----END CERTIFICATE-----";
-        }
-
-        if (strlen($cert2) > 0) { // Successfully got a certificate!
-            /* Create a temporary directory in /var/www/html/pkcs12/ */
-            $tdirparent = getServerVar('DOCUMENT_ROOT') . '/pkcs12/';
-            $polonum = '3';   // Prepend the polo? number to directory
-            if (preg_match('/(\d+)\./',php_uname('n'),$polomatch)) {
-                $polonum = $polomatch[1];
-            }
-            $tdir = tempDir($tdirparent,$polonum);
-            $p12dir = str_replace($tdirparent,'',$tdir);
-            $p12file = $tdir . '/usercred.p12';
-
-            /* Call the openssl pkcs12 program to convert certificate */
-            exec('/bin/env ' .
-                 'CILOGON_PKCS12_PW=' . escapeshellarg($password1) . ' ' .
-                 '/usr/bin/openssl pkcs12 -export ' .
-                 '-passout env:CILOGON_PKCS12_PW ' .
-                 "-out $p12file " .
-                 '<<< ' . escapeshellarg($cert2)
-                );
-
-            /* Verify the usercred.p12 file was actually created */
-            $size = @filesize($p12file);
-            if (($size !== false) && ($size > 0)) {
-                $p12link = 'https://' . getMachineHostname() . '/pkcs12/' .
-                           $p12dir . '/usercred.p12';
-                $p12 = (time()+300) . "\t" . $p12link;
-                setSessionVar('p12',$p12);
-                $log->info('Generated New User Certificate="'.$p12link.'"');
-            } else { // Empty or missing usercred.p12 file - shouldn't happen!
-                setSessionVar('p12error',
-                    'Error creating certificate. Please try again.');
-                deleteDir($tdir); // Remove the temporary directory
-                $log->info("Error creating certificate - missing usercred.p12");
-            }
-        } else { // The myproxy-logon command failed - shouldn't happen!
-            setSessionVar('p12error',
-                'Error! Unable to create certificate.');
-            $log->info("Error creating certificate - myproxy-logon failed");
-        }
-    } else { // Couldn't find the 'dn' PHP session value - shouldn't happen!
-        setSessionVar('p12error',
-            'Missing username. Please enable cookies.');
-        $log->info("Error creating certificate - missing dn session variable");
-    }
-}
-
-/************************************************************************
  * Function   : generateActivationCode                                  *
  * This function is called when the user clicks the "Get New Activation *
  * Code" button.  It calls the GridShib CA functionality to create a    *
@@ -979,27 +847,6 @@ function validateActivationCode() {
         (time() > $tokenexpire)) {
         unsetSessionVar('activation');
     }
-}
-
-/************************************************************************
- * Function   : getMachineHostname                                      *
- * Returns    : The full machine-specific hostname of this host.        *
- * This function is utilized in the formation of the URL for the PKCS12 *
- * credential download link.  It returns a combination of the local     *
- * machine name (the first part of the 'uname') and the HTTP hostname   *
- * (as defined by HOSTNAME in the util.php file).  This usually results *
- * in something like 'polo1.cilogon.org', since polo1 is the local      *
- * machine name, and cilogon.org is the HTTP_HOST name.                 *
- ************************************************************************/
-function getMachineHostname() {
-    $unamesplit = preg_split('/\./',php_uname('n'));
-    $hostname = @$unamesplit[0];
-    $serversplit = preg_split('/\./',HOSTNAME);
-    if (count($serversplit) > 2) { // Delete the first component if more than 2
-        unset($serversplit[0]);
-    }
-    $url = $hostname . '.' . implode('.',$serversplit);
-    return $url;
 }
 
 ?>
