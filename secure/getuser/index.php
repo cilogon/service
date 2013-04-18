a988770935c70871e64aae81fb43d291b4d37a8a
Terry Fleury <tfleury@illinois.edu>
2013-04-18 17:55:46 +0000

    Add new skin config option "usesha1" and new skin "sha1". This supplants the hack that was specific to the OSG skin. Now any skin can specify SHA-1 (instead of SHA-256) when getting a cert from the myproxy server.

--- secure/getuser/index.php
+++ secure/getuser/index.php
@@ -307,8 +307,9 @@ function getCert() {
     } elseif ($loa == 'openid') {
         $port = 7516;
     }
-    /* Special hack for OSG - use SHA-1 version of MyProxy servers */
-    if (strcasecmp(util::getSessionVar('cilogon_skin'),'OSG') == 0) {
+    /* Check if we should use SHA-1 version of MyProxy servers */
+    $usesha1 = $skin->getConfigOption('usesha1');
+    if ((!is_null($usesha1)) && ((int)$usesha1 == 1)) {
         $port--;
     }
 
