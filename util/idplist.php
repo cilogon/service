a7aa47264cc103c888af89706ab86c3055f92fef
Terry Fleury <tfleury@illinois.edu>
2014-07-09 19:09:36 +0000

    Verify that we correctly read in the list of whitelisted IdPs. Also, don't create the idplist.xml file by default, only if we read in whitelisted IdPs correctly.

--- util/idplist.php
+++ util/idplist.php
@@ -7,12 +7,17 @@ require_once('../include/whitelist.php');
 if ($argc == 2) {
     $idpfile = $argv[1];
 
-    $idplist = new idplist($idpfile);
-    $idplist->create();
-    $result = $idplist->write();
-
-    if (!$result) {
-       echo "Error! There was a problem writing to the file '$idpfile'.\n";
+    $idplist = new idplist($idpfile,idplist::defaultInCommonFilename,false);
+    // Make sure we can actually read the set of whitelisted IdPs.
+    // Note that whitelisted IdPs are RE-read during $idplist->create().
+    $whitelist = new whitelist();
+    if (count($whitelist->whitearray) > 0) {
+        $idplist->create();
+        if (!$idplist->write()) {
+           echo "Error! There was a problem writing to the file '$idpfile'.\n";
+        }
+    } else {
+        echo "Error! The list of whitelisted IdPs is empty.\n";
     }
 } else {
     printUsage();
