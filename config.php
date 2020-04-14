17dd6a423ac34f23a61dae40a4b66ac30701b30a
Terry Fleury <tfleury@illinois.edu>
2020-04-14 11:07:15 -0500

    CIL-687 Create skin for flywheel.io.

--- config.php
+++ config.php
@@ -278,7 +278,7 @@ define('FORCE_SKIN_ARRAY', array(
     '%^cilogon:/client_id/38afed60f24d0f627a23edf10314e097%' => 'nih',
     '%^cilogon:/client_id/56727d508077d1402cdcc66714ecf5ef%' => 'orcidfirst',
     '%^cilogon:/client_id/3176b0133ec1efa8090dcf11ad96877f%' => 'MIT',
-    '%^cilogon:/client_id/47fabe917b37f4969efe93ffaa4e510a%' => 'oauthonly',
+    '%^cilogon:/client_id/47fabe917b37f4969efe93ffaa4e510a%' => 'flywheel',
     '%^https://.*\.scimma\.org/.*$%' => 'SCiMMA',
 ));
 
