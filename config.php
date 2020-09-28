88dd7ea8fecde758c1bc8cbc6f51f2e37b1b5871
Terry Fleury <tfleury@illinois.edu>
2020-09-28 14:50:23 -0500

    CIL-832 Make 'NCSA GitLab' OIDC client use NCSA IdP

--- config.php
+++ config.php
@@ -198,6 +198,8 @@ define('BYPASS_IDP_ARRAY', array(
         'https://idp.xsede.org/idp/shibboleth',
     '%^cilogon:/client_id/79d280aef88dd3c97bb1cd92f8217286%' =>
         'https://sts.windows.net/06219a4a-a835-44d5-afaf-3926343bfb89/',
+    '%^cilogon:/client_id/5b59cd113d4fe9d6f1454980bf3ab03d%' =>
+        'https://idp.ncsa.illinois.edu/idp/shibboleth',
 ));
 
 /**
