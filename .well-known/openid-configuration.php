16d1379faa6711cac50e295fa1206297b955ae74
Terry Fleury <tfleury@illinois.edu>
2021-07-25 12:44:44 -0500

    Add response_types_supported and grant_types_supported to match OA4MP.

--- .well-known/openid-configuration.php
+++ .well-known/openid-configuration.php
@@ -11,76 +11,79 @@ if (in_array($localhost, [ 'poloc.ncsa.illinois.edu' ])) {
 header('Content-Type:application/json;charset=utf-8');
 
 echo '{
- "issuer": "https://' , $hostname , '",
- "authorization_endpoint": "https://' , $hostname , '/authorize",
- "device_authorization_endpoint": "https://' , $hostname , '/oauth2/device_authorization",
- "registration_endpoint": "https://' , $hostname , '/oauth2/oidc-cm",
- "token_endpoint": "https://' , $hostname , '/oauth2/token",
- "userinfo_endpoint": "https://' , $hostname , '/oauth2/userinfo",
- "jwks_uri": "https://' , $hostname , '/oauth2/certs",
- "service_documentation": "https://www.cilogon.org/oidc",
- "response_types_supported": [
-  "code"
- ],
- "response_modes_supported": [
-  "query",
-  "fragment",
-  "form_post"
- ],
- "grant_types_supported": [
-  "authorization_code",
-  "refresh_token",
-  "urn:ietf:params:grant_type:token_exchange"
- ],
- "subject_types_supported": [
-  "public"
- ],
- "id_token_signing_alg_values_supported": [
-  "RS256",
-  "RS384",
-  "RS512"
- ],
- "scopes_supported": [
-  "openid",
-  "email",
-  "profile",
-  "org.cilogon.userinfo",
-  "edu.uiuc.ncsa.myproxy.getcert",
-  "offline_access"
- ],
- "token_endpoint_auth_methods_supported": [
-  "client_secret_basic",
-  "client_secret_post"
- ],
- "claims_supported" : [
-  "acr",
-  "affiliation",
-  "aud",
-  "auth_time",
-  "cert_subject_dn",
-  "email",
-  "eppn",
-  "eptid",
-  "exp",
-  "family_name",
-  "given_name",
-  "iat",
-  "idp",
-  "idp_name",
-  "isMemberOf",
-  "iss",
-  "name",
-  "nonce",
-  "oidc",
-  "openid",
-  "ou",
-  "pairwise_id",
-  "preferred_username",
-  "sub",
-  "subject_id",
-  "token_id",
-  "uid",
-  "uidNumber",
-  "voPersonExternalID"
- ]
+  "issuer": "https://' , $hostname , '",
+  "authorization_endpoint": "https://' , $hostname , '/authorize",
+  "device_authorization_endpoint": "https://' , $hostname , '/oauth2/device_authorization",
+  "registration_endpoint": "https://' , $hostname , '/oauth2/oidc-cm",
+  "token_endpoint": "https://' , $hostname , '/oauth2/token",
+  "userinfo_endpoint": "https://' , $hostname , '/oauth2/userinfo",
+  "jwks_uri": "https://' , $hostname , '/oauth2/certs",
+  "service_documentation": "https://www.cilogon.org/oidc",
+  "response_types_supported": [
+    "code",
+    "token",
+    "id_token"
+  ],
+  "response_modes_supported": [
+    "query",
+    "fragment",
+    "form_post"
+  ],
+  "grant_types_supported": [
+    "authorization_code",
+    "refresh_token",
+    "urn:ietf:params:grant_type:token_exchange",
+    "urn:ietf:params:oauth:grant-type:device_code"
+  ],
+  "subject_types_supported": [
+    "public"
+  ],
+  "id_token_signing_alg_values_supported": [
+    "RS256",
+    "RS384",
+    "RS512"
+  ],
+  "scopes_supported": [
+    "openid",
+    "email",
+    "profile",
+    "org.cilogon.userinfo",
+    "edu.uiuc.ncsa.myproxy.getcert",
+    "offline_access"
+  ],
+  "token_endpoint_auth_methods_supported": [
+    "client_secret_basic",
+    "client_secret_post"
+  ],
+  "claims_supported" : [
+    "acr",
+    "affiliation",
+    "aud",
+    "auth_time",
+    "cert_subject_dn",
+    "email",
+    "eppn",
+    "eptid",
+    "exp",
+    "family_name",
+    "given_name",
+    "iat",
+    "idp",
+    "idp_name",
+    "isMemberOf",
+    "iss",
+    "name",
+    "nonce",
+    "oidc",
+    "openid",
+    "ou",
+    "pairwise_id",
+    "preferred_username",
+    "sub",
+    "subject_id",
+    "token_id",
+    "uid",
+    "uidNumber",
+    "voPersonExternalID"
+  ]
 }';
